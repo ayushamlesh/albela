62916a0081a6864c09723f575e5639f0e3f0ada9
Ayush Amlesh <43597067+ayushamlesh@users.noreply.github.com>
2022-11-23 22:07:35 +0530

    Create datetime.js

---
+++ datetime.js
@@ -0,0 +1,12 @@
+<script>21
+        const myDate=new Date();
+        document.write(myDate.getDate());
+        console.log(myDate.getDate());
+        console.log(myDate.getTime());
+        console.log(myDate.getFullYear());
+        console.log(myDate.getDay());
+        console.log(myDate.getMonth());
+        console.log(myDate.getHours());
+        console.log(myDate.getSeconds())
+   
+</script>
