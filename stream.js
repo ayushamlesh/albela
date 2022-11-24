447381e832c7a9e5a5b01c7b266bfd47580bc645
Ayush Amlesh <43597067+ayushamlesh@users.noreply.github.com>
2022-11-24 09:56:34 +0530

    Create stream.js

---
+++ stream.js
@@ -0,0 +1,30 @@
+   
+/*
+const fs = require("fs");
+const http = require("http");
+const server = http.createServer();        
+server.on("request", (req, res) => {
+          
+    //var fs = require("fs");
+          
+    fs.readFile("input.txt", (err, data) => {
+  
+        if (err) return console.error(err);
+          
+        res.end(data.toString());
+          
+    });
+          
+})         
+server.listen(5000);
+        
+        
+          
+*/
+        
+        
+          
+/*
+        
+        
+          
