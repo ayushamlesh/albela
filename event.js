5d3310fbc2c7d2e9f523008c1d86f19e17d13230
Ayush Amlesh <43597067+ayushamlesh@users.noreply.github.com>
2022-11-24 09:51:25 +0530

    Create event.js

---
+++ event.js
@@ -0,0 +1,26 @@
+//we can create,fire and listen for your own event.
+        
+        
+          
+//create event->file it->register in the event.      
+const EventEmitter = require("events");
+const event = new EventEmitter(); // object is created
+        
+        
+          
+event.on("saySomethingNew", () => {
+        
+        
+          
+    console.log("welcome to the new world");
+        
+        
+          
+});//register a listener
+        
+        
+          
+event.emit("saySomethingNew"); //raised an event// it has build in module, called "Events".
+        
+        
+          
